f35fd4b8d5ef190c3ec563e57c1641bb7dd13e9d
Andres <andresdiazanguiano@gmail.com>
2023-07-19 19:55:44 +0200

    v1_final_corregido

--- Caso_practico_final_IBM.py
+++ Caso_practico_final_IBM.py
@@ -30,3 +30,28 @@ def calcular_sumas(matriz):
         
     return suma_filas, suma_columnas
 
+# Para imprimir la suma de filas y columnas
+def imprimir_sumas(suma_filas, suma_columnas):
+    print("Suma de cada fila: ")
+    for suma_fila in suma_filas:
+        print(suma_fila)
+    print("Suma de cada columna:")
+    for suma_columna in suma_columnas:
+        print(suma_columna)
+
+try:
+    N = int(input("Ingrese el tamaño de la matriz cuadrada: "))
+    matriz = generar_matriz(N)
+    print("Matriz generada:")
+    imprimir_matriz(matriz)
+    suma_filas, suma_columnas = calcular_sumas(matriz)
+    imprimir_sumas(suma_filas, suma_columnas)
+except ValueError:
+    print("Error: Por favor, ingrese un valor válido para N.")
+
+# Pruebas unitarias básicas
+# Matriz 2x2 con elementos predefinidos para verificar la suma de filas y columnas.
+matriz_prueba = [[1, 2], [3, 4]]
+suma_filas_prueba, suma_columnas_prueba = calcular_sumas(matriz_prueba)
+assert suma_filas_prueba == [3, 7]
+assert suma_columnas_prueba == [4, 6]
